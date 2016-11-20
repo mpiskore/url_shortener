016a4bdb3d997338c4b116143db298c1b6a61e55
Marek Piskorek <marek.piskorek@gmail.com>
2016-11-20 16:54:04 +0100

    Make the user command invulnerable (partially) on duplicated data from randomuser.me

--- url_mapper/management/commands/create_fake_users.py
+++ url_mapper/management/commands/create_fake_users.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import requests
 
+from django.db.utils import IntegrityError
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
@@ -13,6 +14,8 @@ class Command(BaseCommand):
         parser.add_argument('user_count', nargs='+', type=int)
 
     def handle(self, *args, **options):
+        import ipdb; ipdb.set_trace()
+        duplicated_users = 0
         user_count = options['user_count'][0]
 
         response = requests.get("https://randomuser.me/api/?results={}".format(
@@ -28,8 +31,22 @@ class Command(BaseCommand):
                 'date_joined': datetime.strptime(
                     user['registered'], '%Y-%m-%d %H:%M:%S'),
             }
-            User.objects.create(**kwargs)
+            try:
+                User.objects.create(**kwargs)
+            except IntegrityError:
+                # This user already exists in our database
+                # (This may happen when large amount of users are being
+                # populated to our database in multiple tranches).
+                # We can handle this by counting the number of fails and re-run
+                # the command for missing number of users.
+                duplicated_users += 1
 
         self.stdout.write(self.style.SUCCESS(
-            'Successfully created {} users!'.format(user_count))
+            'Successfully created {} users!'.format(
+                user_count - duplicated_users))
         )
+
+        if duplicated_users > 0:
+            # run the command again for all the users that were duplicated.
+            options['user_count'][0] = duplicated_users
+            self.handle(*args, **options)
