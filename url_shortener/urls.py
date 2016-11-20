78d1df0bb5de581c3667fab81b67f68cf329be93
Marek Piskorek <marek.piskorek@gmail.com>
2016-11-20 16:44:27 +0100

    Remove the admin url.

--- url_shortener/urls.py
+++ url_shortener/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import url
-from django.contrib import admin
 
 from url_mapper.views import HomePage, url_redirect, UrlDetail
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^$', HomePage.as_view(), name='home'),
     url(r'^shortened/(?P<shortened>[A-Za-z0-9]+)/$', HomePage.as_view(), name='result'),
     url(r'^(?P<shortened>[A-Za-z0-9]+)/$', url_redirect, name='redirect'),
