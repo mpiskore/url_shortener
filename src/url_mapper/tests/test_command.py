400ddf43754bc5d29cc4b78efd29dfc67febc1b3
Marek Piskorek <marek.piskorek@gmail.com>
2019-09-28 17:58:10 +0200

    Use pytest in tests

--- src/url_mapper/tests/test_command.py
+++ src/url_mapper/tests/test_command.py
@@ -1,31 +1,32 @@
 import json
-import mock
+import pytest
 import requests
 
 from django.core.management import call_command
-from django.test import TestCase
 from django.utils.six import StringIO
 
+FAKE_USERS_COUNT = 10
 
-class UserFakerTest(TestCase):
-    def faked_users(self, user_count=10):
-        test_data = {}
-        for i in range(user_count):
-            i = "0" + str(i) if i < 10 else str(i)
-            template_dict = {
-                "username": "janedoe_{}".format(i),
-                "first_name": "Jane_{}".format(i),
-                "last_name": "Do{}e".format(i),
-                "password": "johndoelovelove{}".format(i),
-                "date_joined": "20{}-10-22 21:37:11".format(i),
-            }
-            test_data.update(template_dict.copy())
-        return json.dumps(test_data)
 
-    @mock.patch.object(requests, "get")
-    def test_command_output(self, request_mock):
-        user_count = 10
-        request_mock.json.return_value["results"] = self.faked_users(user_count)
-        out = StringIO()
-        call_command("create_fake_users", str(user_count), stdout=out)
-        self.assertIn("Successfully created 10 users!", out.getvalue())
+@pytest.fixture
+def faked_users(user_count=FAKE_USERS_COUNT):
+    test_data = {}
+    for i in range(user_count):
+        i = "0" + str(i) if i < 10 else str(i)
+        template_dict = {
+            "username": "janedoe_{}".format(i),
+            "first_name": "Jane_{}".format(i),
+            "last_name": "Do{}e".format(i),
+            "password": "johndoelovelove{}".format(i),
+            "date_joined": "20{}-10-22 21:37:11".format(i),
+        }
+        test_data.update(template_dict.copy())
+    return json.dumps(test_data)
+
+
+def test_command_output(mocker):
+    mocked_get = mocker.patch.object(requests, "get")
+    mocked_get.json.return_value["results"] = faked_users
+    out = StringIO()
+    call_command("create_fake_users", FAKE_USERS_COUNT, stdout=out)
+    assert f"Successfully created {FAKE_USERS_COUNT} users!" in out.getvalue()
--- src/url_mapper/tests/test_view.py
+++ src/url_mapper/tests/test_view.py
@@ -1,36 +1,35 @@
 import mock
+import pytest
+from django.conf import settings
 
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.urls import reverse
-from django.test import Client, TestCase
 
 from url_mapper.models import UrlMapper
 
 
-class UrlMapperViewTest(TestCase):
-    def setUp(self):
-        self.client = Client()
-        # create a test user
-        User.objects.create(
-            **{"username": "test", "email": "test@example.com", "password": "secret"}
-        )
-        super(UrlMapperViewTest, self).setUp()
+@pytest.fixture
+def user() -> User:
+    return User.objects.create(
+        **{"username": "test", "email": "test@example.com", "password": "secret"}
+    )
 
-    def test_main_page(self):
-        response = self.client.get(reverse("home"))
-        self.assertEqual(response.status_code, 200)
 
-    @mock.patch.object(UrlMapper, "get_shortened_url", return_value="123456")
-    @mock.patch.object(messages, "add_message")
-    def test_success_message(self, add_message_mock, shortenening_mock):
-        response = self.client.post(reverse("home"), {"original_url": "example.com"})
-        self.assertEqual(response.status_code, 302)  # redirected to result
-        test_url = "http://127.0.0.1:8000/123456"
-        add_message_mock.assert_called_once_with(
-            response.wsgi_request,
-            messages.INFO,
-            ('Shortened URL address for example.com is <a href="{0}">{0}</a>').format(
-                test_url
-            ),
-        )
+def test_main_page(client):
+    response = client.get(reverse("home"))
+    assert response.status_code, 200
+
+
+@pytest.mark.usefixtures("db")
+def test_success_message(client, mocker, userk):
+    mocker.patch.object(UrlMapper, "get_shortened_url", return_value="123456")
+    add_message_mock = mocker.patch.object(messages, "add_message")
+    response = client.post(reverse("home"), {"original_url": "example.com"})
+    assert response.status_code, 302  # redirected to result
+    test_url = settings.BASE_URL + "/123456"
+    add_message_mock.assert_called_once_with(
+        response.wsgi_request,
+        messages.INFO,
+        f'Shortened URL address for example.com is <a href="{test_url}">{test_url}</a>'
+    )
