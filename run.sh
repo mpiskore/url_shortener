f9471822750842c37da3ac2b1ffb6325eeba0d6e
Marek Piskorek <marek.piskorek@gmail.com>
2019-09-28 13:00:47 +0200

    Move code to src/, modify Dockerfile and scripts so that test works locally & in container

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+cd src || exit
 python manage.py makemigrations
 python manage.py migrate --noinput
 python manage.py runserver 0.0.0.0:8000
--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 pip install -r requirements_dev.txt
+cd src || exit
 ./manage.py test
